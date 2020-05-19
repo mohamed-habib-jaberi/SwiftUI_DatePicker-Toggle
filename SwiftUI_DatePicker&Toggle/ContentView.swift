f1b65b0d1a742d7b36ad87de5347578007bd5b17
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-05-19 02:14:38 +0200

    Add DatePicker & Toggle

--- SwiftUI_DatePicker&Toggle/ContentView.swift
+++ SwiftUI_DatePicker&Toggle/ContentView.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var isActivatingMessage: String {
+        return "CatNip is " + (isActivate ? "Activate" :  "Desactivate")
+    }
+    var dateFormatter: DateFormatter{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        return dateFormatter
+    }
+    
+    @State private var isActivate = false
+    @State private var pickedDate = Date()
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Toggle("Activate Cat Nip!", isOn: $isActivate )
+            
+            Text(isActivatingMessage)
+                .foregroundColor(isActivate ? .green : .red)
+                .fontWeight(isActivate ? .heavy : .regular)
+            
+            Toggle(isOn: $isActivate){
+                VStack{
+                    
+                    Image("CatNip")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    
+                    Text("Activate CatNip!")
+                    
+                }
+            }
+            
+//            DatePicker(selection: $pickedDate, displayedComponents: [.date]) {
+//
+//                Text("Selected date")
+//            }
+            DatePicker(selection: $pickedDate, in: ClosedRange(uncheckedBounds: (lower: Date(), upper: Date(timeIntervalSinceNow: 900000))), displayedComponents: .date, label: {
+                
+                 Text("Selected date")
+            })
+             Text("\(dateFormatter.string(from: pickedDate))")
+        }
     }
 }
 
